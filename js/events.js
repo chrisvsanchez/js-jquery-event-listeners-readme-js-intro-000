0f7292e070efdf52972fe93037f58ecccb839a8c
Chris Sanchez <chrisvsanchez@gmail.com>
2020-04-27 16:50:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
 //define functions here
+  $('p').on('click' , function getIt(){
+    alert("hey");
+});
+
+$('').on('load',function frameIt(){
+    
+  });
+
+$('').on('keydown', function pressIt(){
+  
+});
+
+$('form').on('submit',function submitIt(){
+  if($('inpit:first').val()=== 'correct'){
+    alert('Your form is going to be submitted now.');
+    return;
+  }
+  alert('you entered the wrong value');
+  return;
+});
 
 $(document).ready(function(){
 
